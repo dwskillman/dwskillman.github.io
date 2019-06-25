7f2d924b3e0b5f2f0adaa4f75349ebef317159ba
dwskillman <dwskillman@gmail.com>
2019-06-24 22:26:47 -0700

    Initial commit
    This is just the first commit.

---
+++ js/data.js
@@ -0,0 +1,99 @@
+var data = [
+	{
+		"question" : "The WORF allows scientists to conduct earth science research and capture high-resolution photographs.",
+		"answers"  : [
+			{"id"  : 0, "text" : "True"},
+			{"id"  : 1, "text" : "False"}
+		],
+		"correct"  : 0,
+		"feedback" : "The optical quality window and the WORF are a perfect blend of art and science, allowing scientists to conduct Earth science research and capture amazing, high-resolution photographs of the Earth."
+	},
+	{
+		"question" : "The WORF launched in 2008 onboard the STS-131 mission of the Space Shuttle Discovery.",
+		"answers"  : [
+			{"id"  : 0, "text" : "True"},
+			{"id"  : 1, "text" : "False"}
+		],
+		"correct"  : 1,
+		"feedback" : "The WORF became a reality in 2010 when it launched on the STS-131 mission onboard the Space Shuttle Discovery."
+	},
+	{
+		"question" : "The WORF is capable of housing a variety of sensors used to make environmental health assessments.",
+		"answers"  : [
+			{"id"  : 0, "text" : "True"},
+			{"id"  : 1, "text" : "False"}
+		],
+		"correct"  : 0,
+		"feedback" : "The WORF houses a variety of sensors within the shirt-sleeve environment inside the space station and serves as a testbed for the development of new sensor technology. These sensors can be used to study atmospheric, oceanic, and surface terrain conditions as well as make environmental health assessments."
+	},
+	{
+		"question" : "Observations made from the WORF can provide essential data on what type of phenomena?",
+		"answers"   : [
+			{ "id" : 0, "text" : "Atmospheric" },
+			{ "id" : 1, "text" : "Oceanic" },
+			{ "id" : 2, "text" : "Planetary" },
+			{ "id" : 3, "text" : "Arctic" }
+		],
+		"correct"  : 0,
+		"feedback" : "One of the benefits having the WORF onboard the space station is that observations made can provide essential data on transient, atmospheric, and geologic phenomena."
+	},
+	{
+		"question" : "Which image depicts a phenomenon observable from the WORF?",
+		"answers"  : [
+			{"id"  : 0, "image" : "images/dust_storm.jpg" },
+			{"id"  : 1, "image" : "images/tornado.jpg" },
+			{"id"  : 2, "image" : "images/tropical_cyclone.jpg" },
+			{"id"  : 3, "image" : "images/earthquake.jpg" }
+		],
+		"correct"  : 2,
+		"feedback" : "The WORF allows crew members to perform Earth and space science research which helps scientists make observations on certain geologic phenomena such as tropical cyclones and volcanic eruptions."
+	},
+	{
+		"question" : "The WORF is about the size of what household appliance?",
+		"answers"  : [
+			{"id"  : 0, "image" : "images/microwave.jpg" },
+			{"id"  : 1, "image" : "images/waterheater.jpg" },
+			{"id"  : 2, "image" : "images/washing_machine.jpg" },
+			{"id"  : 3, "image" : "images/refrigerator.jpg" }
+		],
+		"correct"  : 3,
+		"feedback" : "NASA scientists envisioned a small facility about the size of a large refrigerator that would enhance the capabilities of the large Earth-viewing optical quality window that is onboard the space station."
+	},
+	{
+		"question" : "The _________________ investigation allows for spectroscopic analysis of meteors entering Earth’s upper atmosphere.",
+		"answers"  : [
+			{"id"  : 0, "text" : "National Meteor Laboratory"},
+			{"id"  : 1, "text" : "National Space Laboratory"},
+			{"id"  : 2, "text" : "National Laboratory Meteor "},
+			{"id"  : 3, "text" : "National Laboratory Space"}
+		],
+		"correct"  : 2,
+		"feedback" : "In order for scientists to better understand our local solar system environment, the National Laboratory Meteor investigation allows for spectroscopic analysis of meteors as they enter the Earth’s upper atmosphere."
+	},
+	{
+		"question" : "The WORF's presence on the space station allows its sensors to image the same location or region __________________.",
+		"answers"  : [
+			{"id"  : 0, "text" : "three times in a day"},
+			{"id"  : 1, "text" : "one time over several days"},
+			{"id"  : 2, "text" : "twice a day"},
+			{"id"  : 3, "text" : "multiple times over several days"},
+			{"id"  : 4, "text" : "five times in a day"},
+			{"id"  : 5, "text" : "multiple times in a day"}
+		],
+		"correct"  : 3,
+		"feedback" : "The presence of the WORF on the station allows its sensors to image the same location or region multiple times over several days. This consistency allows for observations that can show how vegetation below may be changing from day to day."
+	},
+	{
+		"question" : "The WORF serves as a testbed for the development of new _______ technology.",
+		"answers"  : [
+			{"id"  : 0, "text" : "astronaut"},
+			{"id"  : 1, "text" : "space"},
+			{"id"  : 2, "text" : "sensor"},
+			{"id"  : 3, "text" : "global positioning"},
+			{"id"  : 4, "text" : "communication"},
+			{"id"  : 5, "text" : "window"}
+		],
+		"correct"  : 2,
+		"feedback" : "The WORF's ability to serve as a testbed for the development of new sensor technology enables scientists to study atmospheric, oceanic, and surface terrain conditions."
+	}
+]
